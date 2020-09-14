fe7682475c0fa53596ddfed238bd73a249a17ab5
katomasahiro10 <katomasahiro10@gmail.com>
2020-09-15 03:34:03 +0900

    modify setup.py
    これでダメなら諦める

--- src/setup.py
+++ src/setup.py
@@ -3,4 +3,4 @@ from Cython.Build import cythonize
 from numpy import get_include # cimport numpy を使うため
 
 ext = Extension("update_weight", sources=["update_weight.pyx"], include_dirs=[get_include()])
-setup(name="update_weight", ext_modules=cythonize([ext]))
+setup(name="update_weight", ext_modules=cythonize([ext]), include_dirs=[get_include()])
